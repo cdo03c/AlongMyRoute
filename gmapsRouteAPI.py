eca312b85a5063f198638a9db7d1356bcbb1db71
cdo03c <cdo03c@gmail.com>
2017-08-22 12:29:43 -0400

    Fixes findStepsWindow function to use directions function from google maps library and update endTime calculation.

--- gmapsRouteAPI.py
+++ gmapsRouteAPI.py
@@ -3,7 +3,7 @@
 """
 Created on Tue Jul 11 16:19:51 2017
 
-@author: obrien
+@author: cdo03c
 """
 
 #Import python libraries
@@ -36,30 +36,29 @@ def findStepsWindow(route, startTime = 7200, endTime = None, twindow = 600):
     and possibly and end time and returns the location of the waypoint(s) along
     the route that falls within time parameters.'''
     
-    #Sets the end Time (eT) variable by testing if the endTime parameter is null
-    #If not null eT is set to the defined endTime plus the time window buffer
-    #If null eT is set to the startTime plus the time window buffer
-    if(endTime):
-        eT = endTime + twindow
-    else:
-        eT = startTime + twindow
-        
-    #Sets the start time (sT) variable parameter by subtracting the time window
-    #from the startTime parameter
-    sT = startTime-twindow
+    #Sets the start time window (sTW) variable parameter by subtracting the 
+    #time window from the startTime parameter
+    sTW = startTime-twindow
+    
+    #Sets endTime to startTime if endTime is null
+    if(not endTime):
+        endTime = startTime
+    #Sets the end time window (eTW) variable parameter by subtracting the 
+    #time window from the endTime parameter
+    eTW = startTime + twindow
     
     #Calls the findSteps function which returns a 
-    steps = findSteps(route, sT, eT)
+    steps = findSteps(route, sTW, eTW)
     
-    buffer = tWindow/5
-    if ((steps[0][1]>sT-2) & (sWindow[0][1]<sT+2)):
-        return steps
+    if ((steps[0][1]>sTW) & (steps[0][1]<startTime)):
+        return(steps)
     else:
-        sL = route.json()['routes'][0]['legs'][0]['steps'][steps[0][0]]['start_location']
-        eL = route.json()['routes'][0]['legs'][0]['steps'][steps[0][0]]['end_location']
-        findStepsWindow(getGMapRoute(origin = '{},{}'.format(sL['lat'],sL['lng']), 
-                             destination ='{},{}'.format(eL['lat'],sL['lng']),
-                             key = ''), startTime = startTime-steps[0][1], endTime = eT-steps[0][1])
+        sL = route[0]['legs'][0]['steps'][steps[0][0]]['start_location']
+        eL = route[0]['legs'][0]['steps'][steps[0][0]]['end_location']
+        return(findStepsWindow(gmaps.directions(origin = '{},{}'.format(sL['lat'],sL['lng']),
+                                             destination ='{},{}'.format(eL['lat'],sL['lng'])),
+                            startTime = startTime-steps[0][1],
+                            endTime = eTW-steps[0][1]))
         
 
 
@@ -85,37 +84,41 @@ window = findStepsWindow(route,
 #route.json()
 
 
-
-#The route call to gmap has a json function that returns a dictionary with the
-#following keys.
-route.json().keys()
-#dict_keys(['routes', 'status', 'geocoded_waypoints'])
-
-#The routes key contains a one element list which contains a dictionary.
-route.json()['routes'][0]
-
-route.json()['routes'][0].keys()
-#dict_keys(['copyrights', 'warnings', 'waypoint_order', 'summary', 'bounds', 'overview_polyline', 'legs'])
-
-#Geospatial extent of the route.
-route.json()['routes'][0]['bounds']
-#{'northeast': {'lat': 41.8781139, 'lng': -77.14263059999999}, 'southwest': {'lat': 38.7586761, 'lng': -87.630549}}
-
-#Legs contains the location data for the journey.
-route.json()['routes'][0]['legs']
-
-#The legs list contains one elements, a dictionary with the following keys.
-route.json()['routes'][0]['legs'][0].keys()
-#dict_keys(['start_address', 'end_address', 'start_location', 'traffic_speed_entry', 'distance', 'steps', 'duration', 'end_location', 'via_waypoint'])
-
-#The steps key in the legs dictionary contains a list of dictionaries for each 
-#of the steps in a particular leg and each step dictionary with the element call
-route.json()['routes'][0]['legs'][0]['steps']
-
-#The API call returns the Google geocodes of the origin and destination.
-route.json()['geocoded_waypoints']
-#[{'place_id': 'ChIJvUBPCmutt4kRxiQmNB4jueA', 'types': ['street_address'], 'geocoder_status': 'OK'}, {'place_id': 'ChIJ7cv00DwsDogRAMDACa2m4K8', 'types': ['locality', 'political'], 'geocoder_status': 'OK'}]
-
-
+# =============================================================================
+# Tests to figure out how data is returned from Google Maps API Queries 
+#
+#
+# #The route call to gmap has a json function that returns a dictionary with the
+# #following keys.
+# route.json().keys()
+# #dict_keys(['routes', 'status', 'geocoded_waypoints'])
+# 
+# #The routes key contains a one element list which contains a dictionary.
+# route.json()['routes'][0]
+# 
+# route.json()['routes'][0].keys()
+# #dict_keys(['copyrights', 'warnings', 'waypoint_order', 'summary', 'bounds', 'overview_polyline', 'legs'])
+# 
+# #Geospatial extent of the route.
+# route.json()['routes'][0]['bounds']
+# #{'northeast': {'lat': 41.8781139, 'lng': -77.14263059999999}, 'southwest': {'lat': 38.7586761, 'lng': -87.630549}}
+# 
+# #Legs contains the location data for the journey.
+# route.json()['routes'][0]['legs']
+# 
+# #The legs list contains one elements, a dictionary with the following keys.
+# route.json()['routes'][0]['legs'][0].keys()
+# #dict_keys(['start_address', 'end_address', 'start_location', 'traffic_speed_entry', 'distance', 'steps', 'duration', 'end_location', 'via_waypoint'])
+# 
+# #The steps key in the legs dictionary contains a list of dictionaries for each 
+# #of the steps in a particular leg and each step dictionary with the element call
+# route.json()['routes'][0]['legs'][0]['steps']
+# 
+# #The API call returns the Google geocodes of the origin and destination.
+# route.json()['geocoded_waypoints']
+# #[{'place_id': 'ChIJvUBPCmutt4kRxiQmNB4jueA', 'types': ['street_address'], 'geocoder_status': 'OK'}, {'place_id': 'ChIJ7cv00DwsDogRAMDACa2m4K8', 'types': ['locality', 'political'], 'geocoder_status': 'OK'}]
+# 
+# 
+# =============================================================================
 #print()
 #print(response.content)
